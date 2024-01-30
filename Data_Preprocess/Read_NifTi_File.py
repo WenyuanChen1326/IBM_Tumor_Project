66df002397e2d8663bb1436eff25eb297efbaf7f
WenyuanChen1326 <cwyuan1010@gmail.com>
2024-01-30 10:39:56 -0800

    added petct_utils

--- Data_Preprocess/Read_NifTi_File.py
+++ Data_Preprocess/Read_NifTi_File.py
@@ -2,21 +2,30 @@ import nibabel as nib
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-
+from utils import *
 # Load the .nii.gz file
-file_path = '/Users/wenyuanchen/Desktop/IBM/Data/PETCT_0b57b247b6/05-02-2002-NA-PET-CT Ganzkoerper  primaer mit KM-42966/SEG.nii.gz'
-img = nib.load(file_path)
+file_path0 = '/Users/wenyuanchen/Desktop/IBM/IBM_Tumor_Project/Data/PETCT_0b57b247b6/05-02-2002-NA-PET-CT Ganzkoerper  primaer mit KM-42966/CTres.nii.gz'
+# file_path = '/Users/wenyuanchen/Desktop/IBM/IBM_Tumor_Project/Data/PETCT_0b57b247b6/05-02-2002-NA-PET-CT Ganzkoerper  primaer mit KM-42966/tumor_0_seg.nii.gz'
+# file_path2 = '/Users/wenyuanchen/Desktop/IBM/IBM_Tumor_Project/Data/PETCT_0b57b247b6/05-02-2002-NA-PET-CT Ganzkoerper  primaer mit KM-42966/tumor_1_seg.nii.gz'
+img =  nib.load(file_path0)
+seg_data = img.get_fdata()
+# img = nib.load(file_path)q
+# img2 = nib.load(file_path2)
 
-# Convert the image data to a numpy array
-data = img.get_fdata()
+# # # Convert the image data to a numpy array
+# data = img.get_fdata()
+# data2 = img2.get_fdata()
+# print(np.all(data==data))
 
 
 # Display a specific slice (change the slice number as needed)
-slice_number = 0 # Example slice number
+# slice_number = 0 # Example slice number
 # plt.imshow(data[:, :, slice_number], cmap='gray')
-plt.imshow(data[:, :, slice_number], cmap='gray', vmin=0, vmax=1)
+# plt.imshow(data[:, :, slice_number], cmap='gray', vmin=0, vmax=1)
 print(img.header)
-# is the dimention in mm? The xyzt_units is 0, which means unknown?
+# # is the dimention in mm? The xyzt_units is 0, which means unknown?
 # print(img.header['pixdim'][1:4].shape)
-# print(np.all(data[:, :, slice_number]==0))
-plt.show()
+# print(np.all(get_connected_components_3D(seg_data)[1] == data2))
+# # print(img.header)
+# # print(np.all(data[:, :, slice_number]==0))
+# plt.show()
--- Data_Preprocess/utils.py
+++ Data_Preprocess/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy import ndimage
 import nibabel as nib
+import matplotlib.pyplot as plt
+import math
 
 def get_connected_components_3D(seg_data):
     # input seg_data is the numpy after reading nifti and get_fdata()
@@ -13,7 +15,8 @@ def get_connected_components_3D(seg_data):
     labled_mask, num_features = ndimage.label(binary_mask)
 
     #assign a unique id to each component
-    unique_ids = np.unique(labled_mask)[1:]
+    # unique_ids = np.unique(labled_mask)[1:]
+    unique_ids = np.unique(labled_mask)
     # TO-DO: can we slice the unique_ids to exclude the first one, which is 0 corresponding to the background?
     # print(unique_ids)
 
@@ -37,42 +40,66 @@ def calculate_tumor_volumes(separate_seg_masks, voxel_dimensions):
         num_voxels = np.sum(mask)
         tumor_voxel_counts.append(num_voxels)
         #the volume of each voxel is the product of its dimensions
-        tumor_volume = num_voxels * np.prod(voxel_dimensions)
+        tumor_volume = num_voxels * np.prod(voxel_dimensions)/1000
         # print(f"Tumor volume: {tumor_volume} cubic mm")
-        tumor_volumes.append(tumor_volume)
+        tumor_volumes.append(np.round(tumor_volume,4))
         # break
     return tumor_voxel_counts, tumor_volumes
 
 def calculate_SUV_value(separate_seg_masks, SUV_data):
-    SUV_values = []
+    SUV_mean_values = []
+    SUV_max_values =[]
+    SUV_min_values = []
     for mask in separate_seg_masks:
         # apply the mask to the SUV data
         tumor_suv_data = SUV_data[mask]
         # calculate the SUV value within the masked region
-        SUV_value = np.sum(tumor_suv_data)
+        SUV_max = np.round(np.max(tumor_suv_data),4)
+        SUV_min = np.round(np.min(tumor_suv_data),4)
+        SUV_mean = np.round(np.mean(tumor_suv_data),4)
         # print(f"Tumor volume: {tumor_volume} cubic mm")
-        SUV_values.append(SUV_value)
-    return SUV_values
+        SUV_max_values.append(SUV_max)
+        SUV_min_values.append(SUV_min)
+        SUV_mean_values.append(SUV_mean)
+    return SUV_mean_values, SUV_max_values, SUV_min_values
+
+
 def main(seg_data, SUV_data, voxel_dimensions):
     separate_seg_masks = get_connected_components_3D(seg_data)
     tumor_voxel_counts, tumor_volumes = calculate_tumor_volumes(separate_seg_masks, voxel_dimensions)
-    SUV_values = calculate_SUV_value(separate_seg_masks, SUV_data)
+    SUV_mean_values, SUV_max_values, SUV_min_values = calculate_SUV_value(separate_seg_masks, SUV_data)
     for idx, volume in enumerate(tumor_volumes):
-        print(f"Tumor {idx+1}: Volume = {volume} cubic mm")
+        print(f"Tumor {idx}: Volume = {volume} cubic cm")
+        print(f"Tumor {idx}: Cubic root Physical Volume = {np.round(math.cbrt(volume),2)} cubic cm")
+
     for idx, voxel_count in enumerate(tumor_voxel_counts):
-        print(f"Tumor {idx+1}: Voxel Count = {voxel_count}")
-    for idx, value in enumerate(SUV_values):
-        print(f"Tumor {idx+1}: SUV Value = {value}")
-    return tumor_voxel_counts, tumor_volumes, SUV_values
+        print(f"Tumor {idx}: Voxel Count = {voxel_count}")
+    for idx, value in enumerate(SUV_mean_values):
+        print(f"Tumor {idx}: SUV Mean Value = {value}")
+    for idx, value in enumerate(SUV_max_values):
+        print(f"Tumor {idx}: SUV Max Value = {value}")
+    for idx, value in enumerate(SUV_min_values):
+        print(f"Tumor {idx}: SUV Min Value = {value}")
+    return tumor_voxel_counts, tumor_volumes, SUV_mean_values, SUV_max_values, SUV_min_values
+
 
 if __name__ == "__main__":
-    seg_file_path = '/Users/wenyuanchen/Desktop/IBM/Data/PETCT_0b57b247b6/05-02-2002-NA-PET-CT Ganzkoerper  primaer mit KM-42966/SEG.nii.gz'
+    # pet_file_path = '/Users/wenyuanchen/Desktop/IBM/IBM_Tumor_Project/Data/PETCT_0b57b247b6/05-02-2002-NA-PET-CT Ganzkoerper  primaer mit KM-42966/PET.nii.gz'
+    seg_file_path = '/Users/wenyuanchen/Desktop/IBM/IBM_Tumor_Project/Data/PETCT_5d553bf6b4/09-16-2001-NA-PET-CT Ganzkoerper  primaer mit KM-78907/SEG.nii.gz'
     seg_img = nib.load(seg_file_path)
     # Convert the image data to a numpy array
     seg_data = seg_img.get_fdata()
-    suv_file_path = '/Users/wenyuanchen/Desktop/IBM/Data/PETCT_0b57b247b6/05-02-2002-NA-PET-CT Ganzkoerper  primaer mit KM-42966/SUV.nii.gz'
+    suv_file_path = '/Users/wenyuanchen/Desktop/IBM/IBM_Tumor_Project/Data/PETCT_5d553bf6b4/09-16-2001-NA-PET-CT Ganzkoerper  primaer mit KM-78907/SUV.nii.gz'
+    print(f"working with {suv_file_path}")
     suv_img = nib.load(suv_file_path)
     # Convert the image data to a numpy array
     suv_data = suv_img.get_fdata()
     voxel_dimensions = seg_img.header['pixdim'][1:4]
+    print(f'voxel_dimensions: {voxel_dimensions}')
+    # print(f"working")
     main(seg_data=seg_data, SUV_data=suv_data, voxel_dimensions=voxel_dimensions)
+    # separate_seg_masks = get_connected_components_3D(seg_data)
+    # # print(f'printing the mask 0')
+    # print(seg_data.shape)
+    # print (separate_seg_masks[0].shape)
+
